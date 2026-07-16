ea9e3dc1701d4d5e2c2698278a54c77a4f31c999
Flassskaa <97349099+AdroadCactus634@users.noreply.github.com>
2026-07-16 05:10:47 +0300

    Update timeUtilities.ts

--- src/timeUtilities.ts
+++ src/timeUtilities.ts
@@ -2,17 +2,7 @@ import { Temporal } from "@js-temporal/polyfill";
 import answerIds from "./datasets/answerIds.json";
 
 export const getAnswerIndexToday = (): number => {
-  const currentTime = Temporal.Now.instant().epochMilliseconds;
-  const startingTime = Temporal.ZonedDateTime.from({
-    year: 2023,
-    month: 8,
-    day: 17,
-    timeZone: "America/Chicago",
-  }).toInstant().epochMilliseconds;
-  const dayIndex = Math.floor(
-    (currentTime - startingTime) / (1000 * 3600 * 24)
-  );
-  return dayIndex % answerIds.length;
+  return Math.floor(Math.random() * answerIds.length);
 };
 
 export const getAnswerIdToday = (): number => {
@@ -20,37 +10,6 @@ export const getAnswerIdToday = (): number => {
   return answerIds[dayIndex];
 };
 
-/**
- * Returns HH:MM:SS until the next question is available
- * Returns NOW if the answerIndex is for a past day
- * @param answerIndex index for the relative day's question
- */
 export const getTimeUntilNextQuestion = (answerIndex: number): string => {
-  const currentTime = Temporal.Now.instant().epochMilliseconds;
-  const startingTime = Temporal.ZonedDateTime.from({
-    year: 2023,
-    month: 8,
-    day: 17,
-    timeZone: "America/Chicago",
-  }).toInstant().epochMilliseconds;
-  const millisInDay = 1000 * 3600 * 24;
-  const timeUntilNextQuestion =
-    millisInDay -
-    currentTime +
-    startingTime +
-    answerIndex * 24 * 60 * 60 * 1000;
-  if (timeUntilNextQuestion < 0) {
-    return "NOW";
-  }
-  const hours = Math.floor(timeUntilNextQuestion / (1000 * 60 * 60));
-  const minutes = Math.floor(
-    (timeUntilNextQuestion - hours * 1000 * 60 * 60) / (1000 * 60)
-  );
-  const seconds = Math.floor(
-    (timeUntilNextQuestion - hours * 1000 * 60 * 60 - minutes * 1000 * 60) /
-      1000
-  );
-  return `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  return "NOW";
 };
